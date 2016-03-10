196f7170a65a76fe9a1b14442b1731181a0a384d
jmart <jmart@DESKTOP-4TH3JSB.EQT.local>
2016-03-10 16:36:05 +0100

    completado actionPerformed();

--- GestorParcAtraccionsBBDD/src/controlador/ControladorCoordinador.java
+++ GestorParcAtraccionsBBDD/src/controlador/ControladorCoordinador.java
@@ -3,6 +3,9 @@ package controlador;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import model.Coordinador;
+import model.ParcAtraccions;
 import vista.*;
 
 public class ControladorCoordinador implements ActionListener {
@@ -48,14 +51,38 @@ public class ControladorCoordinador implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         JButton botonpresionado = (JButton) e.getSource();
-    
+        
+        JFrame formOrigenBoton = (JFrame) botonpresionado.getRootPane().getParent();
+        if (menuCoordinadorsVista != null && formOrigenBoton == menuCoordinadorsVista.getFrame()) {
+            JButton[] listaBotones = menuCoordinadorsVista.getMenuButtons();
+                    for (int i = 0; i < listaBotones.length; i++) {
+            if (listaBotones[i] == botonpresionado) opcioSeleccionada = i;
+            bifurcaOpcio(opcioSeleccionada);
+        }
+        } else if (coordinadorForm != null && formOrigenBoton == coordinadorForm.getFrame()) {
+            if (botonpresionado == coordinadorForm.getbDesar()){
+                ParcAtraccions pActual = ControladorPrincipal.getParcAtraccionsActual();
+                Coordinador coo = new Coordinador(coordinadorForm.gettNif().getText(), 
+                                                   coordinadorForm.gettNom().getText(), 
+                                                    coordinadorForm.gettCognom().getText());
+                pActual.nouCoordinador(coo);
+            } else if (botonpresionado == coordinadorForm.getbSortir()){
+                coordinadorForm.getFrame().setVisible(false);
+            }
+        } else if (coordinadorLlista != null && formOrigenBoton == coordinadorLlista.getFrame()) {
+            if (botonpresionado == coordinadorLlista.getbSortir()){
+                coordinadorLlista.getFrame().setVisible(false);
+            }       
+        }
+
+        
         //Heu de soobrescriure el mètode actionPerformed perquè:
-        // -Es cridar a bifurcaOpcio segons el botó seleccionat del menú coordinadors.
-        //  Penseu que l'opció es correspon amb la posició que el botó ocupa a l'array 
-        //  de botons de menuCoordinadorsVista.
-        // -Si el botó premut és desar del formulari, es crea un nou coordinador amb les dades
-        //  introduides per l'usuari i es guarda en el vector del parc d'atraccions
-        //  registrat (parc d'atraccions actual)
+        // *-Es cridar a bifurcaOpcio segons el botó seleccionat del menú coordinadors.
+        // * Penseu que l'opció es correspon amb la posició que el botó ocupa a l'array 
+        // * de botons de menuCoordinadorsVista.
+        // *-Si el botó premut és desar del formulari, es crea un nou coordinador amb les dades
+        // * introduides per l'usuari i es guarda en el vector del parc d'atraccions
+        // * registrat (parc d'atraccions actual)
         // -Si el botó premut és sortir, de la llista o formulari de coordinadors, 
         //  alehores heu de tornar al menú parc d'atraccions i ammagar la llista
         //  o el formulari, segons sigui la finestra oberta.
